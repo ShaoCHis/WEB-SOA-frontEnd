1b6730110240d31840665e38080d7d9b76be4ae4
vbbbbbbbbbb <64478807+vbbbbbbbbbb@users.noreply.github.com>
2021-11-17 14:50:59 +0800

    处理less 问题

--- hospital/src/router/index.js
+++ hospital/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Register from '@/components/Register'
+import ManagePage from '@/components/Admin/ManagePage'
+import HMainPage from '@/components/Hospital/HMainPage'
+import MainPage from '@/components/Patient/MainPage'
 
 
 Vue.use(Router)
@@ -8,6 +12,10 @@ Vue.use(Router)
 
 export const ConstantRouterMap = [
   { path: '/', name:"Login",component: Login, hidden: true },
+  { path: '/', name:"Register",component: Register, hidden: true },
+  { path: '/Admin/ManagePage', name: 'ManagePage',component: ManagePage},
+  { path: '/Hospital/HMainPage', name: 'HMainPage',component: HMainPage},
+  { path: '/Patient/MainPage', name: 'MainPage',component: MainPage},
 
 
 ]
