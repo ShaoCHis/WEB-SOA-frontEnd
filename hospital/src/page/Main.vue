cc6c302723a43d0adc03ac9fe7fac83ec53a9132
vbbbbbbbbbb <64478807+vbbbbbbbbbb@users.noreply.github.com>
2021-11-21 11:52:47 +0800

    Update Main.vue

--- hospital/src/page/Main.vue
+++ hospital/src/page/Main.vue
@@ -5,52 +5,91 @@
           <el-container>
               
               <div class="tip">
-                <h1>济病通 预约挂号统一平台</h1>
+                <h1>济病通 医院端使用平台</h1>
               </div>
               <el-input style="width:50%;margin-left:15px;margin-right:15px;"
               v-model="hospitalName"
-              placeholder="请输入医院名称"
+              placeholder="请输入....."
             ></el-input>
                 <el-button class="btns" type="primary" icon="el-icon-search">搜索</el-button>
                 <el-button class="btns" type="primary" @click="login">登录/注册 </el-button>
+                <el-avatar icon="el-icon-user-solid" @click.native="showHos()" class="hosInfo"></el-avatar>
           </el-container>
       </el-header>
       <el-container>
         <el-aside width="200px">
-            <el-menu>
-                <el-submenu index="00">
-                    <template slot="title">
-                    <span>预约挂号</span>
-                    </template>
-                </el-submenu>
-                <el-submenu index="01">
-                    <template slot="title">
-                    <span>医院详情</span>
-                    </template>
-                </el-submenu>
-                <el-submenu index="02">
-                    <template slot="title">
-                    <span>预约须知</span>
-                    </template>
-                </el-submenu>
-                <el-submenu index="03">
-                    <template slot="title">
-                    <span>停诊信息</span>
-                    </template>
-                </el-submenu>
-                <el-submenu index="04">
-                    <template slot="title">
-                    <span>查询/取消</span>
-                    </template>
-                </el-submenu>
-            </el-menu>
+            <el-button type="text" @click="showNotice">发布公告</el-button><br/>
+            <el-button type="text" @click="showRoom">查看科室情况</el-button><br/>
+            <el-button type="text" @click="showRes">查看预约情况</el-button><br/>
+            <el-button type="text" @click="showSuspend">停诊信息</el-button><br/>
+            <el-button type="text" @click="showMoney">平台流水</el-button><br/>
         </el-aside>
         <el-container>
-          <el-main>北京协和医院 </el-main>
-          <el-footer>Footer</el-footer>
+          <el-main>
+            <!-- 医院信息 -->
+            <el-dialog
+              title="医院基本信息"
+              :visible.sync="hosData.dialogVisible"
+              width="40%"
+              :before-close="handleCloseHos">
+              医院基本信息
+            </el-dialog>
+            <!-- 发布公告 -->
+            <el-card
+                title="发布公告"
+                v-if="this.hosNotice.dialogVisible"
+                width="40%">
+
+                <div style="height:320px;" :data="hosNotice">
+                  这里是公告板信息
+                </div>
+              </el-card>
+
+            <!-- 查看科室情况 -->
+            <el-card
+                title="科室情况"
+                v-if="this.hosRoom.dialogVisible"
+                width="40%">
+
+                <div style="height:320px;" :data="hosRoom">
+                  这里是科室信息
+                </div>
+              </el-card>
+            <!-- 查看预约情况 -->
+            <el-card
+              title="查看预约情况"
+              v-if="this.hosRes.dialogVisible"
+              width="40%">
+              <div style="height:320px;" :data="hosRes">
+                  查看预约情况
+                </div>
+            </el-card>
+            <!-- 停诊信息 -->
+            <el-card
+              title="查看停诊情况"
+              v-if="this.hosSuspend.dialogVisible"
+              width="40%">
+              <div style="height:320px;" :data="hosSuspend">
+                  查看停诊情况
+                </div>
+            </el-card>
+            <!-- 平台流水 -->
+            <el-card
+              title="平台流水"
+              v-if="this.hosMoney.dialogVisible"
+              width="40%">
+              <div style="height:320px;" :data="hosMoney">
+                  平台流水
+                </div>
+            </el-card>
+          </el-main>
+          <el-footer>联系我们</el-footer>
         </el-container>
       </el-container>
     </el-container>
+    
+    
+    
   </div>
 </template>
 
@@ -59,7 +98,35 @@ export default {
   name: "Main",
   data() {
     return {
-        hospitalName:'',
+      //医院信息
+      hosData:{
+        dialogVisible:false,  //控制el-card是否显示
+        hosName:'',
+        hosID:" ",
+        hosPhone:" ",
+        hosSpecialty:" ",   //医院特长
+        hosDate:" ",        //开院日期
+      },
+      hosNotice:{
+        dialogVisible:false,
+
+      },
+      hosRoom:{
+        dialogVisible:false,
+
+      },
+      hosRes:{
+        dialogVisible:false,
+
+      },
+      hosSuspend:{
+        dialogVisible:false,
+
+      },
+      hosMoney:{
+        dialogVisible:false,
+
+      },
     };
   },
   methods:{
@@ -71,6 +138,51 @@ export default {
     login(){
           this.$router.push({name:'Login'})
       },
+    //医院头像处信息
+    showHos(){
+      this.hosData.dialogVisible=true;
+      
+    },
+    //公告板
+    showNotice(){
+      this.hosNotice.dialogVisible=true;  //控制el-card是否显示
+      this.hosRoom.dialogVisible=false;
+      this.hosRes.dialogVisible=false;
+      this.hosSuspend.dialogVisible=false;
+      this.hosMoney.dialogVisible=false;
+    },
+    //科室信息
+    showRoom(){
+      this.hosRoom.dialogVisible=true;
+      this.hosNotice.dialogVisible=false;
+      this.hosRes.dialogVisible=false;
+      this.hosSuspend.dialogVisible=false;
+      this.hosMoney.dialogVisible=false;
+    },
+    //预约信息
+    showRes(){
+      this.hosRoom.dialogVisible=false;
+      this.hosNotice.dialogVisible=false;
+      this.hosRes.dialogVisible=true;
+      this.hosSuspend.dialogVisible=false;
+      this.hosMoney.dialogVisible=false;
+    },
+    //停诊信息
+    showSuspend(){
+      this.hosRoom.dialogVisible=false;
+      this.hosNotice.dialogVisible=false;
+      this.hosRes.dialogVisible=false;
+      this.hosSuspend.dialogVisible=true;
+      this.hosMoney.dialogVisible=false;
+    },
+    //平台流水
+    showMoney(){
+      this.hosRoom.dialogVisible=false;
+      this.hosNotice.dialogVisible=false;
+      this.hosRes.dialogVisible=false;
+      this.hosSuspend.dialogVisible=false;
+      this.hosMoney.dialogVisible=true;
+    },
   },
 };
 </script>
