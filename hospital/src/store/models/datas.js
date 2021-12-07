9d9ed9ead8de6d988cd33c1d8e4460fdef1d2e76
Lirin <62457732+1951586@users.noreply.github.com>
2021-12-07 15:27:09 +0800

    Updata

--- hospital/src/store/models/datas.js
+++ hospital/src/store/models/datas.js
@@ -1,18 +1,18 @@
-const datas={
-    state:{
-        departData:[]
-        // this.$store.state.datas.departData
+const datas = {
+  state: {
+    departData: []
+    // this.$store.state.datas.departData
+  },
+  mutations: {
+    SET_DEPARTDATA: (state, departData) => {
+      state.departData = departData
     },
-    mutations: { 
-        SET_DEPARTDATA: (state, departData) => {
-          state.departData = departData
-        },
-      },
-    actions:{
-    setDepart({ commit },commitment) {
-        commit('SET_DEPARTDATA',commitment);
+  },
+  actions: {
+    setDepart({ commit }, commitment) {
+      commit('SET_DEPARTDATA', commitment);
     }
-}
+  }
 }
 
 export default datas
