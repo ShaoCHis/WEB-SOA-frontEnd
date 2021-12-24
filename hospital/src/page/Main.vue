af7771e7c2df73f4f071929b737401a3cc2cb2d3
Lirin <62457732+1951586@users.noreply.github.com>
2021-12-24 15:14:21 +0800

    update hospital

--- hospital/src/page/Main.vue
+++ hospital/src/page/Main.vue
@@ -26,90 +26,126 @@
           @click="scrollTop"
         ></el-button>
       </div>
-        
+
       <!-- 页面内容 -->
       <el-container class="page">
-          <div class="shell">
-            <!-- <a href="#" class="box"><i class="iconfont icon-liebiao"></i><span>医院公告</span></a> -->
-            <a class="box" v-if="this.form.focus === 'hosNotice'">
-                <router-link to="/Notice">
-                  <i class="iconfont icon-yiliaojigou"></i><span> 医院公告</span>
-                </router-link>
-                <br />
-              </a>
-            <a class="box" v-else>
-                <router-link to="/Notice">
-                    <i class="iconfont icon-yiliaojigou" @click="() => {
-                        this.form.focus = 'hosNotice';
-                      }"></i><span> 医院公告</span>
-                    </router-link>
-                <br />
-            </a>
+        <div class="shell">
+          <!-- <a href="#" class="box"><i class="iconfont icon-liebiao"></i><span>医院公告</span></a> -->
+          <a class="box" v-if="this.form.focus === 'hosNotice'">
+            <router-link to="/Notice">
+              <i class="iconfont icon-yiliaojigou"></i><span> 医院公告</span>
+            </router-link>
+            <br />
+          </a>
+          <a class="box" v-else>
+            <router-link to="/Notice">
+              <i
+                class="iconfont icon-yiliaojigou"
+                @click="
+                  () => {
+                    this.form.focus = 'hosNotice';
+                  }
+                "
+              ></i
+              ><span> 医院公告</span>
+            </router-link>
+            <br />
+          </a>
 
-            <a class="box" v-if="this.form.focus === 'room'">
-                <router-link to="/RoomInfo">
-                  <i class="iconfont icon-yiliaofuwu"></i><span> 科室信息</span>
-                </router-link>
-                <br />
-            </a>
-            <a class="box" v-else>
-                <router-link to="/RoomInfo">
-                    <i class="iconfont icon-yiliaofuwu" @click="() => {
-                        this.form.focus = 'room';
-                      }"></i><span> 科室信息</span>
-                    </router-link>
-                <br />
-            </a>
-            <!-- <a href="#" class="box"><i class="iconfont icon-cangku"></i><span> 科室信息</span></a> -->
-            <a class="box" v-if="this.form.focus === 'res'">
-                <router-link to="/ReservationStatus">
-                  <i class="iconfont icon-dingdan"></i><span> 预约情况</span>
-                </router-link>
-                <br />
-            </a>
-            <a class="box" v-else>
-                <router-link to="/ReservationStatus">
-                    <i class="iconfont icon-dingdan" @click="() => {
-                        this.form.focus = 'res';
-                      }"></i><span> 预约情况</span>
-                    </router-link>
-                <br />
-            </a>
-            <!-- <a href="#" class="box"><i class="iconfont icon-zhuti_tiaosepan"></i><span>预约信息</span></a> -->
-            <a class="box" v-if="this.form.focus === 'suspend'">
-                <router-link to="/suspendStatus">
-                  <i class="iconfont icon-shujujianguan"></i><span> 停诊信息</span>
-                </router-link>
-                <br />
-            </a>
-            <a class="box" v-else>
-                <router-link to="/suspendStatus">
-                    <i class="iconfont icon-shujujianguan" @click="() => {
-                        this.form.focus = 'suspend';
-                      }"></i><span> 停诊信息</span>
-                    </router-link>
-                <br />
-            </a>
-            <!-- <a href="#" class="box"><i class="iconfont icon-qianbao"></i><span>停诊信息</span></a> -->
-            <a class="box" v-if="this.form.focus === 'money'">
-                <router-link to="/MoneyFlow">
-                  <i class="iconfont icon-renminbi"></i><span> 平台流水</span>
-                </router-link>
-                <br />
-            </a>
-            <a class="box" v-else>
-                <router-link to="/MoneyFlow">
-                    <i class="iconfont icon-renminbi" @click="() => {
-                        this.form.focus = 'money';
-                      }"></i><span> 平台流水</span>
-                    </router-link>
-                <br />
-            </a>
-            <!-- <a href="#" class="box"><i class="iconfont icon-tupian"></i><span>平台流水</span></a> -->
-            <a href="#" class="box"><i class="iconfont icon-erweima"></i><span>QR code</span></a>
-            <a href="#" class="box"><i class="iconfont icon-guanli"></i><span>管理</span></a>
-            <a @click="exit" href="#" class="box"><i class="iconfont icon-yuanchengchuantou"></i><span>退出</span></a>
-          </div>
+          <a class="box" v-if="this.form.focus === 'room'">
+            <router-link to="/RoomInfo">
+              <i class="iconfont icon-yiliaofuwu"></i><span> 科室信息</span>
+            </router-link>
+            <br />
+          </a>
+          <a class="box" v-else>
+            <router-link to="/RoomInfo">
+              <i
+                class="iconfont icon-yiliaofuwu"
+                @click="
+                  () => {
+                    this.form.focus = 'room';
+                  }
+                "
+              ></i
+              ><span> 科室信息</span>
+            </router-link>
+            <br />
+          </a>
+          <!-- <a href="#" class="box"><i class="iconfont icon-cangku"></i><span> 科室信息</span></a> -->
+          <a class="box" v-if="this.form.focus === 'res'">
+            <router-link to="/ReservationStatus">
+              <i class="iconfont icon-dingdan"></i><span> 预约情况</span>
+            </router-link>
+            <br />
+          </a>
+          <a class="box" v-else>
+            <router-link to="/ReservationStatus">
+              <i
+                class="iconfont icon-dingdan"
+                @click="
+                  () => {
+                    this.form.focus = 'res';
+                  }
+                "
+              ></i
+              ><span> 预约情况</span>
+            </router-link>
+            <br />
+          </a>
+          <!-- <a href="#" class="box"><i class="iconfont icon-zhuti_tiaosepan"></i><span>预约信息</span></a> -->
+          <a class="box" v-if="this.form.focus === 'suspend'">
+            <router-link to="/suspendStatus">
+              <i class="iconfont icon-shujujianguan"></i><span> 停诊信息</span>
+            </router-link>
+            <br />
+          </a>
+          <a class="box" v-else>
+            <router-link to="/suspendStatus">
+              <i
+                class="iconfont icon-shujujianguan"
+                @click="
+                  () => {
+                    this.form.focus = 'suspend';
+                  }
+                "
+              ></i
+              ><span> 停诊信息</span>
+            </router-link>
+            <br />
+          </a>
+          <!-- <a href="#" class="box"><i class="iconfont icon-qianbao"></i><span>停诊信息</span></a> -->
+          <a class="box" v-if="this.form.focus === 'money'">
+            <router-link to="/MoneyFlow">
+              <i class="iconfont icon-renminbi"></i><span> 平台流水</span>
+            </router-link>
+            <br />
+          </a>
+          <a class="box" v-else>
+            <router-link to="/MoneyFlow">
+              <i
+                class="iconfont icon-renminbi"
+                @click="
+                  () => {
+                    this.form.focus = 'money';
+                  }
+                "
+              ></i
+              ><span> 平台流水</span>
+            </router-link>
+            <br />
+          </a>
+          <!-- <a href="#" class="box"><i class="iconfont icon-tupian"></i><span>平台流水</span></a> -->
+          <a href="#" class="box"
+            ><i class="iconfont icon-erweima"></i><span>QR code</span></a
+          >
+          <a href="#" class="box"
+            ><i class="iconfont icon-guanli"></i><span>管理</span></a
+          >
+          <a @click="exit" href="#" class="box"
+            ><i class="iconfont icon-yuanchengchuantou"></i><span>退出</span></a
+          >
+        </div>
 
         <!-- 指示信息栏（所有栏目都会显示的信息） -->
         <!-- avatar放logo -->
@@ -143,12 +179,9 @@
             class="hosPic"
             :src="hosData.image"
             style="margin-left: 20%; width: 50%; height: 50%; float: center"
-            
           />
         </div>
-        <el-button type="primary" style="margin-left: 40%"
-          >上传头像</el-button
-        >
+        <el-button type="primary" style="margin-left: 40%">上传头像</el-button>
       </div>
 
       <div
@@ -234,8 +267,8 @@
 
 <script>
 import { getHospInfo } from "../api/hospital";
-import "../style/iconfont.css"
-import "../style/main.css"
+import "../style/iconfont.css";
+import "../style/css/main.css";
 
 export default {
   name: "Main",
@@ -321,10 +354,7 @@ export default {
         .catch((_) => {});
       this.hosDataVisible = false;
     },
-    modifyInfo(){
-
-    },
-
+    modifyInfo() {},
   },
   destroyed() {
     window.removeEventListener("scroll", this.handleScroll);
@@ -333,181 +363,5 @@ export default {
 </script>
 
 <style lang="less" scoped>
-div.sticky {
-  position: sticky;
-  top: 0;
-  z-index: 999;
-  padding: 5px;
-  height: 70px;
-  background-color: rgba(255, 255, 255, 0.25);
-  box-shadow: 0 3px 16px rgba(0, 0, 0, 0.2);
-  //   background-color: rgba(0, 0, 0, 0.25);
-  //   box-shadow: 0 10px 25px rgba(255, 255, 255, 1);
-  // border: 2px solid #4CAF50;
-}
-.page-header {
-  background-color: white;
-}
-
-.topic {
-  color: cornflowerblue;
-  font-size: 42px;
-  font-weight: bold;
-  text-align: left;
-  padding-top: 1%;
-  padding-left: 1.5%;
-  position: inherit;
-  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
-}
-.btns {
-  display: flex;
-  justify-content: center;
-  height: 5%;
-}
-// .el-header{
-//   line-height: 10%;
-// }
-.avatar-head {
-  position: relative;
-  margin-top: 2%;
-}
-
-.name-head {
-  color: cornflowerblue;
-  position: relative;
-  font-size: 28px;
-  font-weight: bold;
-  margin-left: 2%;
-  position: inherit;
-}
-
-.level-head {
-  font-size: 16px;
-  color: gray;
-  position: inherit;
-  margin-top: 2.7%;
-  margin-left: 2%;
-}
-
-.choose-hos {
-  color: black;
-  position: relative;
-  text-align: center;
-  width: 100px;
-  line-height: 20px;
-  font-size: 20px;
-  font-weight: bold;
-}
-
-.choose-room {
-  color: black;
-  position: relative;
-  text-align: center;
-  width: 100px;
-  line-height: 20px;
-  font-size: 20px;
-  font-weight: bold;
-}
-
-/* 医院信息弹窗中内容 */
-.hosInfo {
-  position: fixed;
-  font-size: 17px;
-  right: 5%;
-}
-.el-footer {
-  // background-color: #ffffffe7;
-  color: #333;
-  text-align: center;
-  // line-height: 30px;
-}
-
-.el-aside {
-  color: black;
-  text-align: center;
-  position: fixed;
-  margin-top: 5%;
-  line-height: 60px;
-  font-size: 70px;
-}
-.search-hospital {
-  padding-top: 1%;
-}
-.el-main {
-  padding-top: 5%;
-  margin-left: 17%;
-  margin-right: 7%;
-  color: #333;
-  text-align: center;
-  position: relative;
-}
-.el-input {
-  width: 50%;
-  margin-top: 15px;
-  margin-bottom: 15px;
-}
-
-.text-css {
-  font-size: 20px;
-  color: black;
-}
-
-.text-css-focus {
-  font-size: 20px;
-  color: cornflowerblue;
-  font-weight: bold;
-}
-.ivu-row {
-  flex-flow: row wrap;
-}
-.ivu-col-span-12 {
-  display: block;
-  flex: 0 0 50%;
-  max-width: 50%;
-}
-.ivu-col {
-  position: relative;
-  max-width: 100%;
-  min-height: 1px;
-}
-.ivu-form-item {
-  margin-bottom: 24px;
-  vertical-align: top;
-  zoom: 1;
-}
-.ivu-form-item-content {
-  position: relative;
-  line-height: 32px;
-  font-size: 14px;
-}
-.ivu-input-wrapper {
-  display: inline-block;
-  width: 100%;
-  position: relative;
-  vertical-align: middle;
-  line-height: normal;
-}
-.ivu-drawer-body {
-  width: 100%;
-  height: 100%;
-  padding: 16px;
-  font-size: 20px;
-  line-height: 1.5;
-  word-wrap: break-word;
-  position: flex;
-  overflow: auto;
-}
-.background {
-  background-image: url("../assets/backgrond.jpg");
-  background-size: 100%, 100%;
-  padding-bottom: 30%;
-  position: fixed;
-  height: 100%;
-  width: 100%;
-}
-.footer {
-  position: fixed;
-  left: 35%;
-  bottom: 5%;
-}
+@import "../style/css/main.less";
 </style>
