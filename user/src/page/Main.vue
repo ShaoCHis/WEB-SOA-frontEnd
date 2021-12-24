0ecc44e3d84da82822ed1748e68d7494d0c61480
Lirin <62457732+1951586@users.noreply.github.com>
2021-12-24 19:06:55 +0800

    update

--- user/src/page/Main.vue
+++ user/src/page/Main.vue
@@ -15,27 +15,7 @@ export default {
   components: { myheader,myfooter, Myfooter },
   name: "Main",
   data: {
-    loginTemp: {
-      position: "absolute",
-      top: "18px",
-      right: "5.5%",
-    },
-    avatarHos: {
-      position: "absolute",
-      top: "18px",
-      right: "2%",
-    },
-    searchInput: {
-      width: "66%",
-      top: "3%",
-      left: "18%",
-      position: "absolute",
-    },
-    searchBtn: {
-      position: "absolute",
-      top: "18px",
-      right: "12%",
-    },
+   
   },
 };
 </script>
