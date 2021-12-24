c1d48ebc889060d1d666c741a3f0fd8cdf762523
Lirin <62457732+1951586@users.noreply.github.com>
2021-12-24 17:39:16 +0800

    update

---
+++ user/src/page/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mainContainer">
+    <div class="header">
+    <el-button type="text" class="topic">医济达</el-button><br />
+    <!--导航栏-->
+    <el-menu
+      :default-active="activeIndex"
+      mode="horizontal"
+      class="nav"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">处理中心</el-menu-item>
+    </el-menu>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  data: {},
+};
+</script>
+
+<style lang="less" scoped>
+@import "../style/css/main.less";
+</style>
