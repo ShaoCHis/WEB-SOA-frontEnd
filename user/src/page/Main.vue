ef9c8d983baa718fc6e1030000057a2fe68431f5
Mr-Cu <56495695+Mr-Cu@users.noreply.github.com>
2021-12-25 12:18:56 +0800

    医院列表

--- user/src/page/Main.vue
+++ user/src/page/Main.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="mainContainer">
     <myheader></myheader>
-<div class="contentContainer">
-    <!-- 首页走马灯 -->
-     <div class="carousel-container">
-      <div class="home-carousel">
-        <el-carousel indicator-position="inside" height="400px">
-          <el-carousel-item v-for="item in carousel_list" :key="item.img">
-            <img :src="item.img" style="width: 100%; height: 100%" />
-          </el-carousel-item>
-        </el-carousel>
+    <div class="contentContainer">
+      <!-- 首页走马灯 -->
+      <div class="carousel-container">
+        <div class="home-carousel">
+          <el-carousel indicator-position="inside" height="400px">
+            <el-carousel-item v-for="item in carousel_list" :key="item.img">
+              <img :src="item.img" style="width: 100%; height: 100%" />
+            </el-carousel-item>
+          </el-carousel>
+        </div>
       </div>
-    </div>
 
-    
       <!-- 根据科室选择医院 -->
       <div class="label">
         <div class="label-left"></div>
@@ -56,11 +55,11 @@
                 </div>
               </div>
             </div>
-
-            <!-- {{ item.name }} -->
           </el-tab-pane>
+          <!-- 分页 -->
           <div class="block">
             <el-pagination
+            background
               @size-change="handleSizeChange"
               @current-change="handleCurrentChange"
               :page-size="pageSize"
@@ -72,8 +71,66 @@
           </div>
         </el-tabs>
       </div>
-      <!-- 分页 -->
-      <!-- <div class="pagination"> -->
+
+      <!-- 医院列表 -->
+      <div class="label">
+        <div class="label-left"></div>
+        <div class="label-name">医院列表</div>
+      </div>
+
+      <div class="hospital-choose">
+        <el-tabs type="border-card">
+          <el-tab-pane
+            v-for="(item, index) in sortType"
+            :key="index"
+            :label="item.name"
+          >
+            <!-- <div class="sort">
+              {{ item.name }}
+            </div> -->
+            <div
+              class="department-choose-hospital"
+              v-for="(item, index) in currentHospital"
+              :key="index"
+            >
+              <div class="inner-choose">
+                <el-divider></el-divider>
+                <div class="hospital-image">
+                  <img :src="item.image" style="width: 100%; height: 100%" />
+                </div>
+                <div class="hospital-content">
+                  <div class="hospital-name">{{ item.name }}</div>
+                  <div class="hospital-level">{{ item.level }}</div>
+                </div>
+                <div class="enter-hospital">
+                  <div class="inner-enter" @click="goToHospitalPage()">
+                    <a
+                      style="text-decoration: none"
+                      href="https://ak.hypergryph.com/index"
+                      target="_blank"
+                      >选择医院</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </el-tab-pane>
+          <!-- 分页 -->
+          <div class="block">
+            <el-pagination
+            background
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :page-size="pageSize"
+              :current-page.sync="currentPage"
+              layout="total,prev, pager, next, jumper"
+              :total="hospital.length"
+            >
+            </el-pagination>
+          </div>
+        </el-tabs>
+      </div>
+
     </div>
     <myfooter></myfooter>
   </div>
@@ -116,7 +173,22 @@ export default {
   },
   data() {
     return {
-       carousel_list: [
+      sortType:[
+        // 筛选方法
+        {
+          id:"1",
+          name:"综合",
+        },
+        {
+          id:"2",
+          name:"距离远近",
+        },
+        {
+          id:"3",
+          name:"医院等级",
+        },
+      ],
+      carousel_list: [
         {
           id: "1",
           img: require("@/assets/carousel3.jpg"),
