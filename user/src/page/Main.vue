ac084aaff9e8810d5b25be0c74dfa645ac0d4282
Lirin <62457732+1951586@users.noreply.github.com>
2021-12-25 13:23:52 +0800

    update

--- user/src/page/Main.vue
+++ user/src/page/Main.vue
@@ -1,6 +1,53 @@
 <template>
   <div class="mainContainer">
-    <myheader></myheader>
+    <div class="header-container">
+      <div class="wrapper">
+        <!-- logo -->
+        <div class="left-wrapper v-link">
+          <img
+            style="width: 50px"
+            width="50"
+            height="50"
+            src="../../static/images/logo.png"
+          />
+          <span class="text">医济达预约挂号平台</span>
+        </div>
+
+        <!-- 导航栏 -->
+        <el-menu class="el-menu-demo" mode="horizontal">
+          <el-menu-item index="1"
+            ><router-link style="text-decoration: none" to="/"
+              >首页</router-link
+            ></el-menu-item
+          >
+          <el-menu-item index="2"
+            ><router-link style="text-decoration: none" to="/reservation"
+              >挂号</router-link
+            ></el-menu-item
+          >
+        </el-menu>
+        <el-input class="search" placeholder="请输入医院名称" />
+        <el-button circle icon="el-icon-search" type="primary"></el-button>
+        <div class="avatar">
+          <el-button
+            v-show="this.ifLogin == false"
+            class="loginBtn"
+            type="primary"
+            @click="login()"
+            >登录/注册</el-button
+          >
+          <el-avatar
+            v-show="this.ifLogin == true"
+            size="large"
+            fit="fit"
+            :src="avatar"
+          ></el-avatar>
+        </div>
+        <!-- 右侧 -->
+        <div class="right-wrapper"></div>
+      </div>
+    </div>
+
     <div class="contentContainer">
       <!-- 首页走马灯 -->
       <div class="carousel-container">
@@ -59,7 +106,7 @@
           <!-- 分页 -->
           <div class="block">
             <el-pagination
-            background
+              background
               @size-change="handleSizeChange"
               @current-change="handleCurrentChange"
               :page-size="pageSize"
@@ -118,7 +165,7 @@
           <!-- 分页 -->
           <div class="block">
             <el-pagination
-            background
+              background
               @size-change="handleSizeChange"
               @current-change="handleCurrentChange"
               :page-size="pageSize"
@@ -130,19 +177,21 @@
           </div>
         </el-tabs>
       </div>
-
     </div>
     <myfooter></myfooter>
   </div>
 </template>
 
 <script>
-import myfooter from "../layout/myfooter";
-import Myfooter from "../layout/myfooter.vue";
+import myfooter from "../layout/myfooter.vue";
 import myheader from "../layout/myheader.vue";
+import "@/style/iconfont.css";
+import "@/style/app.css";
+import "@/style/main.css";
+import "@/style/chunk.css";
 
 export default {
-  components: { myheader, myfooter, Myfooter },
+  components: { myheader, myfooter },
   name: "Main",
   mounted() {
     this.currentHospital = [];
@@ -154,6 +203,9 @@ export default {
     // console.log(this.currentHospital);
   },
   methods: {
+    login() {
+      this.$router.push({ name: "Login" });
+    },
     goToHospitalPage() {},
     handleSizeChange(val) {
       this.pageSize = val;
@@ -173,19 +225,20 @@ export default {
   },
   data() {
     return {
-      sortType:[
+      ifLogin: false, //用户是否已经登录
+      sortType: [
         // 筛选方法
         {
-          id:"1",
-          name:"综合",
+          id: "1",
+          name: "综合",
         },
         {
-          id:"2",
-          name:"距离远近",
+          id: "2",
+          name: "距离远近",
         },
         {
-          id:"3",
-          name:"医院等级",
+          id: "3",
+          name: "医院等级",
         },
       ],
       carousel_list: [
@@ -266,6 +319,19 @@ export default {
 
 <style lang="less" scoped>
 @import "../style/css/main.less";
+.thequit {
+  cursor: pointer;
+}
+.search {
+  width: 500px;
+}
+.avatar {
+  margin-right: -150px;
+}
+.loginBtn {
+  display: flex;
+  position: relative;
+}
 </style>
 
 
