22a93f6b14d4208c767e2d711c6351fd8cc48d60
Mr-Cu <56495695+Mr-Cu@users.noreply.github.com>
2021-12-25 11:11:15 +0800

    根据科室选择医院

--- user/src/page/Main.vue
+++ user/src/page/Main.vue
@@ -2,20 +2,167 @@
   <div class="mainContainer">
     <myheader></myheader>
 
+    <div class="contentContainer">
+      <!-- 根据科室选择医院 -->
+      <div class="label">
+        <div class="label-left"></div>
+        <div class="label-name">根据科室选择医院</div>
+      </div>
+
+      <div class="department-choose">
+        <el-tabs type="border-card">
+          <el-tab-pane
+            v-for="(item, index) in department"
+            :key="index"
+            :label="item.name"
+          >
+            <!-- <div class="sort">
+              {{ item.name }}
+            </div> -->
+            <div
+              class="department-choose-hospital"
+              v-for="(item, index) in currentHospital"
+              :key="index"
+            >
+              <div class="inner-choose">
+                <el-divider></el-divider>
+                <div class="hospital-image">
+                  <img :src="item.image" style="width: 100%; height: 100%" />
+                </div>
+                <div class="hospital-content">
+                  <div class="hospital-name">{{ item.name }}</div>
+                  <div class="hospital-level">{{ item.level }}</div>
+                </div>
+                <div class="enter-hospital">
+                  <div class="inner-enter" @click="goToHospitalPage()">
+                    <a
+                      style="text-decoration: none"
+                      href="https://ak.hypergryph.com/index"
+                      target="_blank"
+                      >选择医院</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- {{ item.name }} -->
+          </el-tab-pane>
+          <div class="block">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :page-size="pageSize"
+              :current-page.sync="currentPage"
+              layout="total,prev, pager, next, jumper"
+              :total="hospital.length"
+            >
+            </el-pagination>
+          </div>
+        </el-tabs>
+      </div>
+      <!-- 分页 -->
+      <!-- <div class="pagination"> -->
+    </div>
     <myfooter></myfooter>
   </div>
 </template>
 
 <script>
-import Myfooter from '../layout/myfooter.vue';
-import myheader from '../layout/myheader.vue';
-import "@/style/css/main.css";
+import myfooter from "../layout/myfooter";
+import Myfooter from "../layout/myfooter.vue";
+import myheader from "../layout/myheader.vue";
 
 export default {
-  components: { myheader, Myfooter },
+  components: { myheader, myfooter, Myfooter },
   name: "Main",
-  data: {
-   
+  mounted() {
+    this.currentHospital = [];
+    for (var i = 0; i < this.pageSize; i++) {
+      if (this.hospital[this.pageSize * (this.currentPage - 1) + i] != null)
+        this.currentHospital[i] =
+          this.hospital[this.pageSize * (this.currentPage - 1) + i];
+    }
+    // console.log(this.currentHospital);
+  },
+  methods: {
+    goToHospitalPage() {},
+    handleSizeChange(val) {
+      this.pageSize = val;
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      console.log(`当前页: ${val}`);
+      this.currentHospital = [];
+      for (var i = 0; i < this.pageSize; i++) {
+        if (this.hospital[this.pageSize * (this.currentPage - 1) + i] != null)
+          this.currentHospital[i] =
+            this.hospital[this.pageSize * (this.currentPage - 1) + i];
+      }
+      console.log(this.currentHospital);
+    },
+  },
+  data() {
+    return {
+      currentHospital: [],
+      pageSize: 2,
+      currentPage: 1,
+      hospital: [
+        {
+          name: "北京协和医院",
+          level: "三甲",
+          image: require("../assets/back.jpeg"),
+          attribute4: "",
+          attribute5: "",
+        },
+        {
+          name: "柳州人民医院",
+          level: "三甲",
+          image: require("../assets/back.jpeg"),
+          attribute4: "",
+          attribute5: "",
+        },
+        {
+          name: "重庆人民医院",
+          level: "三甲",
+          image: require("../assets/back.jpeg"),
+          attribute4: "",
+          attribute5: "",
+        },
+        {
+          name: "金华人民医院",
+          level: "三甲",
+          image: require("../assets/back.jpeg"),
+          attribute4: "",
+          attribute5: "",
+        },
+        {
+          name: "南宁人民医院",
+          level: "三甲",
+          image: require("../assets/back.jpeg"),
+          attribute4: "",
+          attribute5: "",
+        },
+      ],
+      department: [
+        {
+          name: "外科",
+        },
+        {
+          name: "内科",
+        },
+        {
+          name: "中医科",
+        },
+        {
+          name: "肿瘤科",
+        },
+        {
+          name: "儿科",
+        },
+      ],
+    };
   },
 };
 </script>
@@ -23,3 +170,5 @@ export default {
 <style lang="less" scoped>
 @import "../style/css/main.less";
 </style>
+
+
