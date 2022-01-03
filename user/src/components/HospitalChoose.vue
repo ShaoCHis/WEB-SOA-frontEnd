a586e4aa56e42cbc654034decf268b86e7fe59b3
Lirin <62457732+1951586@users.noreply.github.com>
2022-01-03 15:16:39 +0800

    fix bugs

--- user/src/components/HospitalChoose.vue
+++ user/src/components/HospitalChoose.vue
@@ -1,52 +1,80 @@
 <template>
   <div class="hospital-choose">
-    <el-tabs type="border-card"  @tab-click="handleClick">
-      <el-tab-pane
-        v-for="(item, index) in sortType"
-        :key="index"
-        :label="item.name"
-      >
-      <template>
+    <el-tabs type="border-card" @tab-click="handleClick">
+      <el-tab-pane v-for="item in sortType" :key="item" :label="item.name">
+        <template v-if="item.type == 0">
           <el-row :gutter="20">
-          <el-col
-            :span="8"
-            v-for="(item, index) in currentHospital"
-            :key="index"
-            class="department-choose-hospital"
-          >
-          <div @click="goToHospitalPage(item.id)">
-              <div class="hospital-image">
-              <img :src="item.image" style="width: 100%; height: 100%" />
-            </div>
-            <div class="hospital-content">
-              <div class="hospital-name">{{ item.name }}</div>
-              <div class="hospital-level">{{ item.level }}</div>
-            </div>
-            </div>
-          </el-col>
-        </el-row>
-      </template>
-        
+            <el-col
+              :span="8"
+              v-for="hospital in currentHospital"
+              :key="hospital"
+              class="department-choose-hospital"
+            >
+              <div @click="goToHospitalPage(hospital.id)">
+                <div class="hospital-image">
+                  <img
+                    :src="hospital.image"
+                    style="width: 100%; height: 100%"
+                  />
+                </div>
+                <div class="hospital-content">
+                  <div class="hospital-name">{{ hospital.name }}</div>
+                  <div class="level">
+                    <div class="hospital-level">{{ hospital.level }}</div>
+                  </div>
+                </div>
+              </div>
+            </el-col>
+          </el-row>
+        </template>
+        <template v-if="item.type == 1">
+          <el-row :gutter="20">
+            <el-col
+              :span="8"
+              v-for="hospital in currentHospital"
+              :key="hospital.level"
+              class="department-choose-hospital"
+            >
+              <div @click="goToHospitalPage(hospital.id)">
+                <div class="hospital-image">
+                  <img
+                    :src="hospital.image"
+                    style="width: 100%; height: 100%"
+                  />
+                </div>
+                <div class="hospital-content">
+                  <div class="hospital-name">{{ hospital.name }}</div>
+                  <div class="level">
+                    <div class="hospital-level">{{ hospital.level }}</div>
+                  </div>
+                </div>
+              </div>
+            </el-col>
+          </el-row>
+        </template>
       </el-tab-pane>
       <div class="block">
-        <el-pagination background
+        <el-pagination
+          background
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
           :page-size="pageSize"
           :current-page.sync="currentPage"
           layout="total,prev, pager, next, jumper"
-          :total="hospital.length">
+          :total="hospital.length"
+        >
         </el-pagination>
       </div>
     </el-tabs>
   </div>
 </template>
 <script>
-import {getHospList} from "../api/main"
-import {getMap} from "../utils/map"
+import { getHospList } from "../api/main";
+import { getMap } from "../utils/map";
 
 export default {
   name: "HospitalChoose",
+  created() {},
   mounted() {
     this.initPage(0);
     this.currentHospital = [];
@@ -54,30 +82,32 @@ export default {
       if (this.hospital[this.pageSize * (this.currentPage - 1) + i] != null)
         this.currentHospital[i] =
           this.hospital[this.pageSize * (this.currentPage - 1) + i];
-    };
+    }
     // this.currentPage=1;
   },
   methods: {
     handleClick(tab) {
+      console.log(tab.get)
       this.initPage(tab.index);
     },
     goToHospitalPage(id) {
       // console.log(id);
-      sessionStorage.setItem("selectedHosID",id);
+      sessionStorage.setItem("selectedHosID", id);
       // sessionStorage.getItem("selectedHosID")
-      this.$router.push({name:'Hospital'});
+      this.$router.push({ name: "Hospital" });
     },
-    initPage(index){
-      getHospList().then((response)=>{
-        // this.hospital=[],
-        this.hospital=response.data;
-        this.hospital.forEach((element,index) => {
-          element.level=getMap(element.level);
+    initPage(index) {
+      getHospList()
+        .then((response) => {
+          // this.hospital=[],
+          this.hospital = response.data;
+          this.hospital.forEach((element, index) => {
+            element.level = getMap(element.level);
+          });
+        })
+        .catch((error) => {
+          console.log(error);
         });
-      })
-      .catch((error)=>{
-        console.log(error);
-      })
     },
     handleSizeChange(val) {
       this.pageSize = val;
@@ -99,8 +129,8 @@ export default {
       currentHospital: [],
       pageSize: 3,
       currentPage: 1,
-      allHospital:[],
-      hospital:[],
+      allHospital: [],
+      hospital: [],
       //  [
       //   {
       //     name: "北京协和医院",
@@ -143,14 +173,12 @@ export default {
         {
           id: "1",
           name: "综合",
+          type: 0,
         },
         {
           id: "2",
-          name: "距离远近",
-        },
-        {
-          id: "3",
           name: "医院等级",
+          type: 1,
         },
       ],
     };
